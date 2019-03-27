d52361dda86c86b4d8fd052bef2bf5b7e1acd9ac
lBetterManl <yhc8023tm@foxmail.com>
2019-03-27 19:33:33 +0800

    v1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # spring-batch
 spring-batch学习Demo
+
+> SpringBoot的启动类必须放在顶层包下，这样才能扫描注解
---
+++ src/com/hanson/SpringBatchApplication.java
@@ -0,0 +1,24 @@
+/**
+ * Project: spring-batch
+ * File: SpringBatchApplication.java
+ * Package: com.hanson.batch
+ * Date: 2019年3月27日 下午7:12:27
+ * Copyright (c) 2019, hanson.yan@qq.com All Rights Reserved.
+ */
+package com.hanson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>Description: TODO</p>
+ * @author Hanson
+ * @since  JDK 1.8
+ * @time 2019年3月27日 下午7:12:27
+ */
+@SpringBootApplication
+public class SpringBatchApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchApplication.class, args);
+	}
+}
